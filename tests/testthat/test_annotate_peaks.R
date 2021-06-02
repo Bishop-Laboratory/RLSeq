f11cf717eebe4d261eb891f19904b9c0f0c97075
Simon Levy <silevy91@gmail.com>
2021-06-02 15:04:50 -0500

    minor changes to annotate_peaks.R and test_annotate_peaks.R

--- tests/testthat/test_annotate_peaks.R
+++ tests/testthat/test_annotate_peaks.R
@@ -1,7 +1,8 @@
 
 test_that(desc = "Test annotate_peaks", {
   expect_type(
-    RSeqR::annotatePeaks("ERX2277510_E-MTAB-6318DRIP_mOHT_hg38.unstranded.bed"),
+    RSeqR::annotatePeaks(ChIPpeakAnno::toGRanges("ERX2277510_E-MTAB-6318DRIP_mOHT_hg38.unstranded.bed",
+                                                 format = "BED", header = FALSE)),
     "character"
   )
 })
