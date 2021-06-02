f47f55a1417f4d382e8498c1e6587173b9415db1
Simon Levy <silevy91@gmail.com>
2021-06-02 14:03:15 -0500

    initial commit for branch
    Former-commit-id: c44e5157815a1c99c692e4070a0b6bc005df7b38 [formerly c44e5157815a1c99c692e4070a0b6bc005df7b38 [formerly c44e5157815a1c99c692e4070a0b6bc005df7b38 [formerly 71db83ad8de3fac5638f905a265eb0647d3317bd]]]
    Former-commit-id: 96f098377c35dc529ce729fe933fa804559c29df
    Former-commit-id: 2aa75b0858faaba3d28b8a4a34020d34d6a8832c
    Former-commit-id: 547a29d10819ed93f99b1789b66bf9e6d4367de8

---
+++ R/annotate_peaks.R
@@ -0,0 +1,40 @@
+#' Annotate Peaks
+#'
+#' Annotates DRIP-Seq peaks with gene-level information
+#'
+#' @param peaks A .bed file containing DRIP-Seq peaks
+#' @return A .bed file containing annotated DRIP-Seq peaks
+#' @export
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+#BiocManager::install("EnsDb.Hsapiens.v86")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("ChIPpeakAnno")
+
+#library(tidyverse)
+#library(ChIPpeakAnno)
+#library(EnsDb.Hsapiens.v86)
+#library(org.Hs.eg.db)
+
+#bed <- "R/ERX2277510_E-MTAB-6318DRIP_mOHT_hg38.unstranded.bed"
+#peaks <- ChIPpeakAnno::toGRanges(bed, format = "BED", header = FALSE)
+
+annotatePeaks <- function(bed) {
+  peaks <- ChIPpeakAnno::toGRanges(bed, format = "BED", header = FALSE)
+
+  annoData <- toGRanges(EnsDb.Hsapiens.v86)
+
+  anno <- ChIPpeakAnno::annotatePeakInBatch(peaks, AnnotationData = annoData, output = 'overlapping', select = 'all')
+
+  anno <- ChIPpeakAnno::addGeneIDs(anno, orgAnn = "org.Hs.eg.db", feature_id_type = "ensembl_gene_id", IDs2Add = c("symbol"))
+
+  anno_df <- as.data.frame(anno)
+  names(anno_df)[1] <- '#Seqnames'
+  anno_df['peak'] <- make.unique(anno_df[['peak']], sep = '_')
+
+  return(anno_df)
+}
+
+#res <- annotatePeaks(peaks)
