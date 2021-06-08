13eb308e9d8a417fb82963ca7ee39e58388c3738
millerh1 <millerh1@livemail.uthscsa.edu>
2021-06-08 00:14:20 -0500

    Added CI and templating generator

---
+++ R/data.R
@@ -0,0 +1,2 @@
+#' Results of running RSeq on SRX1025890
+"SRX1025890"
---
+++ R/generateReport.R
@@ -0,0 +1,24 @@
+#' Generate report
+#'
+#' Generates RSeqR Quality Report
+#'
+#' @param data A named list containing `annotated_peaks`, `feature_overlaps`,
+#' and `rlfs_results`
+#' @param outputFile A path indicating the report output HTML file. 
+#' @return NULL
+#' @examples
+#' 
+#' RSeqR::makeReport(RSeqR::SRX1025890, output_file = "report.html")
+#' 
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+#' @export
+makeReport <- function(data, output_file = "RSeqReport.html", ...) {
+  template <- system.file("Rmd", "reportTemplate.Rmd", package = "RSeqR")
+  rmarkdown::render(template, 
+                    params = data, 
+                    output_format = "html_document",
+                    output_dir = normalizePath(dirname(output_file)),
+                    output_file = output_file)
+  return(NULL)
+}
