e51ce1d3411b0f25da38fc5ebde393904078e7e7
millerh1 <millerh1@livemail.uthscsa.edu>
2021-06-29 15:52:36 -0500

    added utils.R and tests

--- R/data.R
+++ R/data.R
@@ -1,2 +1,9 @@
-#' Results of running RSeq on SRX1025890
+#' List. Results of running RSeq on SRX1025890
 "SRX1025890"
+
+#' GRanges. Peaks of running RSeq on SRX1025890
+"SRX1025890_peaks"
+
+#' data.frame. Contains informaiton about the genes available in UCSC database.
+#' Result of running RSeqR:::buildAvailableGenomes()
+"available_genomes"
