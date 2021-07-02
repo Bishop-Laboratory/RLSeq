fa5377bff0d72cae8d4e2ba5020a2d8a7b10139b
millerh1 <millerh1@livemail.uthscsa.edu>
2021-07-01 20:20:35 -0500

    passed checks
    Former-commit-id: 0e69cdf4fc56adda50c94c5eeb7da15a5fe550eb [formerly 0e69cdf4fc56adda50c94c5eeb7da15a5fe550eb [formerly 49b8292428c04765d67eb99dee42864b91df53ae]]
    Former-commit-id: c32b8f2d96cdfbcafd73c598795a96b1d230d6dc
    Former-commit-id: 07ea9110cf164f09e915dcf2ce5f168bc9761c44

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,7 @@
-#' Get RLFS
-#'
-#' Helper function which obtains RLFS for a given genome.
-#' @param genome the UCSC genome for which to download RLFS
-#' @return A GRanges object of RLFS.
-getRLFS <- function(genome) {
-  RLFS <- regioneR::toGRanges(paste0(
-    'https://rmapdb-data.s3.us-east-2.amazonaws.com/rlfs-beds/',
-    genome, '.rlfs.bed'))
-  return(RLFS)
-}
-
-
 #' Check if URL exists
 #' @param url URL to check
 #' @return logical. TRUE if status code 200, FALSE if not
+#' @export
 urlExists <- function(url) {
   identical(
     httr::status_code(
@@ -30,6 +18,7 @@ urlExists <- function(url) {
 #' Helper function which downloads chrom sizes from UCSC for a genome.
 #' @param genome the UCSC genome for which to download chrom sizes
 #' @return A tibble containing chrom sizes
+#' @export
 getChromSizes <- function(genome) {
   chrom_sizes <- suppressMessages(readr::read_tsv(paste0(
     'http://hgdownload.soe.ucsc.edu/goldenPath/',
@@ -43,6 +32,7 @@ getChromSizes <- function(genome) {
 #' Helper function that checks whether a genome has RLFS available
 #' @param genome the UCSC genome name to check
 #' @return A logical, TRUE if available, FALSE if not
+#' @export
 checkRLFSAnno <- function(genome) {
   return(
     urlExists(
@@ -59,6 +49,7 @@ checkRLFSAnno <- function(genome) {
 #' Helper function that retrieves RLFS
 #' @param genome the UCSC genome name to retrieve RLFS for
 #' @return A GRanges object with RLFS for that species.
+#' @export
 getRLFSAnno <- function(genome) {
   
   # Check if annotations available first
@@ -70,12 +61,12 @@ getRLFSAnno <- function(genome) {
   return(
     regioneR::toGRanges(
       as.data.frame(
-        readr::read_tsv(
+        suppressMessages(readr::read_tsv(
           paste0(
             "https://rmapdb-data.s3.us-east-2.amazonaws.com/rlfs-beds/", 
             genome, ".rlfs.bed"
           ),
-          col_names = FALSE)
+          col_names = FALSE))
       )
     )
   )
@@ -86,6 +77,7 @@ getRLFSAnno <- function(genome) {
 #' Helper function which checks the chrom sizes info from UCSC
 #' @param genome the UCSC genome for which to download chrom sizes
 #' @return logical. TRUE if status code 200, FALSE if not
+#' @export
 checkGenes <- function(genome) {
   return(
     urlExists(
@@ -102,6 +94,7 @@ checkGenes <- function(genome) {
 #' Helper function which checks whether homer annotations are available
 #' @param genome the UCSC genome for which to download chrom sizes
 #' @return logical. TRUE if status code 200, FALSE if not
+#' @export
 checkHomer <- function(genome) {
   return(
     urlExists(
@@ -112,199 +105,3 @@ checkHomer <- function(genome) {
 }
 
 
-#' builds the conda env through reticulate
-#' @param envName name for new conda env
-#' @param ... other arguments passed to reticulate::conda_create()
-#' @return File path to created environment
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
-buildCondaEnv <- function(envName, ...) {
-  
-  
-  # Create env and install
-  reticulate::conda_create(envname = envName, ...)
-  
-  # Get Env Path
-  envPath <- reticulate::conda_list() %>%
-    dplyr::filter(name == envName) %>%
-    dplyr::mutate(python = gsub(python, pattern = "/bin/python", replacement = "")) %>%
-    dplyr::pull(python)
-  
-  return(envPath)
-}
-
-
-#' Get list of effective genome sizes
-#' @param genome the UCSC genome name to check
-#' @param envPath the path to conda env containing "khmer"
-#' @return A data frame containing the effective genome sizes.
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
-getEffectiveGenomeSizes <- function(genome, 
-                                    envPath,
-                                    lengths = c(36, 50, 75, 100, 125, 
-                                                150, 200, 250, 300)) {
-  
-  fasta_file <- paste0("ftp://hgdownload.soe.ucsc.edu/goldenPath/",
-                       genome, "/bigZips/", genome, ".fa.gz")
-  outFile <- file.path(tempdir(), paste0(genome, ".fa.gz"))
-  
-  # Download genome fasta if not exists
-  if (! file.exists(outFile)) {
-    download.file(fasta_file, destfile = outFile)
-  } 
-  
-  # Find eff genome size for all lengths
-  sizes <- lapply(lengths, getEffGenSize,
-                  faFile=outFile,
-                  envPath=envPath)
-  names(sizes) <- paste0("eff_genome_size_", lengths, "bp")
-  sizedf <- data.frame(sizes) %>%
-    dplyr::mutate(UCSC_orgID = genome)
-  
-  return(sizedf)
-}
-
-
-#' Get effective genome size
-#' @param len The read length for which to obtain genome sizes
-#' @param faFile The genome fasta file to analyze
-#' @param envPath The path to the conda env with khmer installed
-#' @param condaPath The path to the conda binary. Default = reticulate::conda_binary()
-#' @return A data frame containing the effective genome sizes.
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
-getEffGenSize <- function(len,
-                          faFile,
-                          envPath) {
-  
-  # Get back to binary
-  condaPath <- reticulate::conda_binary()
-  
-  # This line sources the conda shell script, activates the khmerenv, and runs unique-kmers.py
-  cmd <- paste0(". ", gsub(condaPath, pattern = "/bin/conda", 
-                                replacement = "/etc/profile.d/conda.sh"), 
-                " && conda activate ", 
-                envPath, " && unique-kmers.py -k ", len, " -R ",
-                faFile, "_", len, ".txt ", faFile)
-  # If already run, then not needed to run again
-  if (! file.exists(paste0(faFile, "_", len, ".txt"))) {
-    system(cmd)
-  } else {
-    print(paste0("Already ran length: ", len))
-  }
-  # Get the part of the unique-kmers.py output with the eff genome size
-  lines <- readLines(paste0(faFile, "_", len, ".txt"))
-  lines <- lines[grep(x = lines,"number of unique k-mers: ")]
-  size <- gsub(lines, pattern = "number of unique k-mers: \t([0-9]+)$",
-                   replacement = "\\1")
-  return(as.numeric(size))
-}
-
-
-#' Build Available Genomes
-#' Helper Function which builds the UCSC available genomes dataset
-#' @param test if TRUE, will only build info set for first 2 genomes in UCSC.
-#' @param ... arguments passed to `getEffectiveGenomeSizes()`
-#' @return A data frame of avaialable genomes and corresponding info. 
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
-buildAvailableGenomes <- function(test=FALSE, ...) {
-  
-  envName <- buildCondaEnv(packages = "khmer", 
-                           envName = "khmerEnv",
-                           channel = c("bioconda", "conda-forge"))
-  
-  # from http://rstudio-pubs-static.s3.amazonaws.com/562103_092b7264f392482e827440cf1521363c.html
-  api_genome <- restfulr::RestUri("http://api.genome.ucsc.edu/list")
-  response <- restfulr::read(api_genome$ucscGenomes)[['ucscGenomes']]
-  
-  # Wrangle the data from UCSC Genome API
-  available_genomes <- do.call(rbind.data.frame, response) %>%
-    tibble::rownames_to_column(var = "UCSC_orgID") %>%
-    dplyr::group_by(UCSC_orgID) %>%
-    dplyr::mutate(genes_available = checkGenes(UCSC_orgID),
-                  homer_anno_available = checkHomer(UCSC_orgID),
-                  year = as.numeric(gsub(x = description, 
-                                         replacement = "\\1",
-                                         pattern = ".+ ([0-9]+) \\(.+")))
-  # If testing, only use small genomes
-  smallgen <- c("eboVir3", "wuhCor1")
-  if (test) {
-    available_genomes <- dplyr::filter(available_genomes, .data$UCSC_orgID %in% smallgen)
-  }
-  
-  # Get the effective genome sizes
-  eff_gen_sizes <- available_genomes %>%
-    dplyr::pull(UCSC_orgID) %>%
-    lapply(function(genome) {
-      getEffectiveGenomeSizes(genome = genome, envPath = envName, ...)
-    }) %>%
-    dplyr::bind_rows()
-  
-  # Bind final table and return
-  return(
-    dplyr::left_join(available_genomes, eff_gen_sizes, by = "UCSC_orgID")
-  )
-}
-
-
-#' Build Genome Masks
-#' Helper Function which builds GRanges of masked genomes for RegioneR
-#' @return A named list of GRanges object with the masked chrom sizes. 
-buildGenomeMasks <- function() {
-  
-  # Find genomes which have masked genomes available
-  available_genomes <- BSgenome::available.genomes()
-  masked_genomes <- available_genomes[grep(available_genomes, 
-                                           pattern = ".+\\.masked")]
-  
-  # Download any masked genomes which are not already available
-  to_install <- masked_genomes[! masked_genomes %in% installed.packages()]
-  if (length(to_install) > 0) {
-    BiocManager::install(to_install, ask = FALSE)
-  }
-  
-  # Get the masked genomes
-  genomes <- gsub(masked_genomes, replacement = "\\1",
-                  pattern = "^BSgenome\\.[a-zA-Z]+\\.UCSC\\.([a-zA-Z0-9]+)\\.masked$")
-  maskLst <- lapply(genomes, getMask2)
-  names(maskLst) <- genomes
-  
-  return(maskLst)
-  
-}
-
-#' Build Mask for Genome as GRanges
-#' Adapted from `regioneR::getMask()` in the 
-#' @return A GRanges object with the masked chrom sizes. 
-getMask2 <- function(genome) {
-  gn <- regioneR::characterToBSGenome(genome)
-  chrs <- as.character(GenomicRanges::seqnames(GRanges(GenomeInfoDb::seqinfo(gn))))
-  bsgenome <- gn
-  chr.masks <- sapply(chrs, function(chr) {
-    mm <- Biostrings::masks(bsgenome[[chr]])
-    if(is.null(mm)) {
-      return(NULL)
-    } else {
-      mm <- Biostrings::collapse(mm)[[1]]
-      return(mm)
-    }})
-  
-  chr.masks <- sapply(chrs, function(chr) {
-    if(is.null(chr.masks[[chr]])) {
-      return(NULL)
-    } else {
-      return(GenomicRanges::GRanges(seqnames=S4Vectors::Rle(rep(chr, length(chr.masks[[chr]]))), ranges=chr.masks[[chr]]))
-    }
-  })
-  #Combine the mask for each chromosome into a single mask
-  mask <- GenomicRanges::GRanges(seqinfo = seqinfo(bsgenome))
-  for(chr in chrs) {
-    if(!is.null(chr.masks[[chr]])) {
-      suppressWarnings(mask <- c(mask, chr.masks[[chr]]))
-    }
-  }
-  
-  return(mask)
-}
