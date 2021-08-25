fb462e327e879d031ebca9e49e2e9dab6d4a76e8
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-24 22:28:22 -0500

    Added predictCondition
    Former-commit-id: faef70eda6bb3d132fdedebae8df12e90cdce2ea [formerly faef70eda6bb3d132fdedebae8df12e90cdce2ea [formerly d02615737420078271ffadefc3ad46b9d97e4060]]
    Former-commit-id: 20ffeb0668f293a140a82f04e0d558be550583a5
    Former-commit-id: 927e223bd2137bbe80b57d55b388333a30e9a221

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' GRanges. Peaks of running RSeq on SRX1025890
 "SRX1025890_peaks"
 
-#' data.frame. Contains informaiton about the genes available in UCSC database.
+#' data.frame. Contains information about the genes available in UCSC database.
 #' Result of running RSeqR:::buildAvailableGenomes()
 "available_genomes"
 
@@ -12,6 +12,14 @@
 #' See data-raw/masked_genomes.R for details.
 "genomeMasks"
 
-#' List of list of tibbles. Containins genomic annotations for use with RSeqR.
+#' List of list of tibbles. Containing genomic annotations for use with RSeqR.
 #' See data-raw/anotationLst.R for details.
 "annotationLst"
+
+#' A Caret model used for preprocessing datasets in predictCondition().
+"prepFeatures"
+
+#' A Caret model used for predicting whether a peakset resembles 
+#' "case" (normal) or "control" (RNaseH1-like) conditions. 
+#' Used in predictionCondition().
+"fftModel"
