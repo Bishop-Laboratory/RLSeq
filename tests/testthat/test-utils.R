7f7c56353d87b9a9ac051ff4832b08c6b11b03e8
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-25 12:25:12 -0500

    Added new dependencies to fix GH action errors
    Former-commit-id: fe3755fbeef9d6730e9b6ef654897b8290d5345f [formerly fe3755fbeef9d6730e9b6ef654897b8290d5345f [formerly 893d0e157309b79b058df3f450dd2db33caa6dfc [formerly 521a835bc9347a4e08cb178c4a974af7fef51b83] [formerly 109ff52829af96c24799f82ec8850a5e3d57143a]]]
    Former-commit-id: 5f13e1267fbeb248a6557e77dd7c093d0e0548cb
    Former-commit-id: 0a93bb9035a83a1558a9043437468f5cb525fda9
    Former-commit-id: e324200cd5563cd1c0b38514335f5f3d3b63c1ee
    Former-commit-id: fdb65b77518054834bd5b32e2d7d26c6158a5aaa

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -22,3 +22,19 @@ test_that("liftUtil works", {
   expect_s4_class(RSeqR:::liftUtil(RSeqR::SRX1025890_peaks_hg19, "hg19", "hg38"),
                   "GRanges")
 })
+
+test_that(desc = "Test that toBed returns a list object and writes an output file", {
+  # Create temporary file for output
+  file <- tempfile()
+  # Check that the file does not exist before running the funciton
+  expect_false(file.exists(paste0(file, ".bed")))
+  # Write .bed file and ascertain that toBed returns a list type object
+  expect_type(
+    RSeqR::grangesToBed(RSeqR::SRX1025890_peaks, write = TRUE, filename = file),
+    "list"
+  )
+  # Check that the output file now exists
+  expect_true(file.exists(paste0(file, ".bed")))
+  # Delete the output file
+  file.remove(paste0(file, ".bed"))
+})
