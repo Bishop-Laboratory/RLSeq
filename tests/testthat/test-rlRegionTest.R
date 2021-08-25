43962611900b5ee0427ce422009d421fc93a0635
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-25 14:45:23 -0500

    Working on CRAN checks
    Former-commit-id: d6e2e91b89554287d995788552aa360423954e82 [formerly d6e2e91b89554287d995788552aa360423954e82 [formerly d961abe340e4531b179c87f01e15b290e0c4ab7b [formerly 82db8766864409de7fc721f987e76246ced51361] [formerly 59e7eabf32607d86f91f7f70bd9d5f77a19185c5]]]
    Former-commit-id: 0e2b68b937144b9ecbf4a8c10553f2905c633af6
    Former-commit-id: f0fa4a866aa042cc7a0a6b57ca476bb041dd3df2
    Former-commit-id: 8d98986f37452796780dd788cea8c7b58fb1ce98
    Former-commit-id: ad2822d01cf84e9ed05def807ebbf3edd8379bd1

--- tests/testthat/test-rlRegionTest.R
+++ tests/testthat/test-rlRegionTest.R
@@ -1,4 +1,4 @@
-test_that(desc = "Feature Enrich returns a tibble", {
+test_that(desc = "RL Region Test returns as list", {
   expect_type (
     RSeqR::rlRegionTest(RSeqR::SRX1025890_peaks, genome="hg38"),
     "list"
