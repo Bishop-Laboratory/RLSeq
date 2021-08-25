d3a9cbc6d086ba21dfc0118462f058b6ac300a2c
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-25 10:54:40 -0500

    added lift, updated gene annotation, and passed new tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,19 +1,24 @@
 test_that("getRLFSAnno works", {
-  expect_s4_class(getRLFSAnno("hg38"), "GRanges")
+  expect_s4_class(RSeqR:::getRLFSAnno("hg38"), "GRanges")
 })
 
 test_that("urlExists works", {
-  expect_true(urlExists("www.google.com"))
+  expect_true(RSeqR:::urlExists("www.google.com"))
 })
 
 test_that("getChromSizes works", {
-  expect_s3_class(getChromSizes("hg38"), class="tbl")
+  expect_s3_class(RSeqR:::getChromSizes("hg38"), class="tbl")
 })
 
 test_that("checkRLFSAnno works", {
-  expect_true(checkRLFSAnno("hg38"))
+  expect_true(RSeqR:::checkRLFSAnno("hg38"))
 })
 
-test_that("checkGenes works", {
-  expect_true(checkGenes("hg38"))
+test_that("getChain works", {
+  expect_s4_class(RSeqR:::getChain("hg19", "hg38"), "Chain")
+})
+
+test_that("liftUtil works", {
+  expect_s4_class(RSeqR:::liftUtil(RSeqR::SRX1025890_peaks_hg19, "hg19", "hg38"),
+                  "GRanges")
 })
