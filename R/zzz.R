6c23f6ea65544a696f82a91c64ca3d95d370f22a
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-26 10:44:57 -0500

    Updating manifest
    Former-commit-id: 693a5777c5cfc9faf6fced349d737c7cf97b85b9
    Former-commit-id: c7c58be17b9460980cadd440b79697f9714ed9d1

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,12 @@
   # From https://stackoverflow.com/questions/49056642/how-to-make-variable-available-to-namespace-at-loading-time/49094961
   BASE_UCSC <- 'http://hgdownload.soe.ucsc.edu/goldenPath/'
   RLFS_BED_URL <- "https://rmapdb-data.s3.us-east-2.amazonaws.com/rlfs-beds/"
+  RMAP_BW_URL <- "https://rmapdb-data.s3.us-east-2.amazonaws.com/bigwigs/rseq-coverage-unstranded/"
+  RMAP_PEAKS_URL <- "https://rmapdb-data.s3.us-east-2.amazonaws.com/macs2-peaks-unstranded/"
+  RMAP_PEAKS_S3 <- "s3://rmapdb-data/macs2-peaks-unstranded/"
   assign("BASE_UCSC", BASE_UCSC, envir = parent.env(environment()))
   assign("RLFS_BED_URL", RLFS_BED_URL, envir = parent.env(environment()))
+  assign("RMAP_BW_URL", RMAP_BW_URL, envir = parent.env(environment()))
+  assign("RMAP_PEAKS_URL", RMAP_PEAKS_URL, envir = parent.env(environment()))
+  assign("RMAP_PEAKS_S3", RMAP_PEAKS_S3, envir = parent.env(environment()))
 }
