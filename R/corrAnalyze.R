d34259bcf20014226e3c103d6f01c5848aee5d3a
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-25 19:53:54 -0500

    Finished updating the gsSignal and corrAnalyze scripts & passing tests

--- R/corrAnalyze.R
+++ R/corrAnalyze.R
@@ -3,37 +3,22 @@
 #' Finds the pairwise correlation in signal around gold-standard R-Loop sites 
 #' between the query sample and the RMapDB database.
 #'
-#' @param coverage The results list from running analyzeRLFS().
+#' @param coverage The path to a coverage (.bigWig/.bw) file (can be a URL).
 #' @param genome The UCSC genome ID to use. (Currently only "hg38" is supported)
 #' @return A named list containing the results of correlation analysis.
 #' @examples
 #'
-#' URL <- paste0("https://rmapdb-data.s3.us-east-2.amazonaws.com/bigwigs/",
-#'               "rseq-coverage-unstranded/SRX1025890_TC32_NT_DRIP.hg38.bw")
-#' BW_FILE <- "SRX1025890.bw"
-#' download.file(URL, destfile=BW_FILE)
-#' result <- RSeqR::corrAnalyze(BW_FILE, genome="hg38")
+#' BW_URL <- paste0("https://rmapdb-data.s3.us-east-2.amazonaws.com/bigwigs/",
+#'                  "rseq-coverage-unstranded/SRX1025890_hg38.bw")
+#' result <- RSeqR::corrAnalyze(BW_URL, genome="hg38")
 #' 
 #' @importFrom dplyr %>%
 #' @importFrom rlang .data
 #' @export
 corrAnalyze <- function(coverage, genome="hg38") {
   
-  # Get the locations of the gs sites
-  positions <- rownames(RSeqR::gsSignalRMapDB) 
-  positions <- tibble::tibble(location = positions) %>%
-    dplyr::mutate(seqnames = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
-                                  replacement = "\\1"),
-                  start = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
-                               replacement = "\\2"),
-                  end = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
-                             replacement = "\\3")) %>%
-    dplyr::select(-.data$location) %>%
-    GenomicRanges::makeGRangesFromDataFrame()
-  
-  # Read in the bigWig file using these locations
-  bw <- rtracklayer::import(con = rtracklayer::BigWigFile(coverage), 
-                            selection = positions)
+  # Get the signal around GS R-loop sites
+  bw <- getGSSignal(coverage, genome=genome)
   
   # Wrangle BW into tibble
   bw <- bw %>%
@@ -42,20 +27,23 @@ corrAnalyze <- function(coverage, genome="hg38") {
     dplyr::rename(chrom = .data$seqnames) %>%
     dplyr::mutate(chrom = as.character(.data$chrom))
   
-  # Wrangle positions into tibble
-  posTbl <- positions %>%
-    as.data.frame() %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(chrom = .data$seqnames) %>%
-    dplyr::mutate(chrom = as.character(.data$chrom))
+  # Get positions
+  positions <- RSeqR::gsSignalRMapDB %>%
+    dplyr::select(.data$location) %>%
+    dplyr::mutate(chrom = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
+                               replacement = "\\1"),
+                  start = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
+                               replacement = "\\2"),
+                  end = gsub(.data$location, pattern = "(.+)_(.+)_(.+)",
+                             replacement = "\\3")) %>%
+    dplyr::select(-.data$location) %>%
+    dplyr::mutate(dplyr::across(c("start", "end"), as.integer))
   
   # Summarize across gs intervals with valr
-  bwMap <- valr::bed_map(x = posTbl, y = bw, value = sum(score))
+  bwMap <- valr::bed_map(x = positions, y = bw, value = sum(score))
   
   # Combine with the original matrix
   combinedMat <- gsSignalRMapDB %>%
-    as.data.frame() %>%
-    tibble::rownames_to_column(var = "location") %>%
     dplyr::inner_join(
       bwMap %>%
         dplyr::mutate(location = paste0(.data$chrom, "_",
