fadfbee6e5ca5facbbc31cf93d74bc628bfabc2f
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-25 19:53:54 -0500

    Finished updating the gsSignal and corrAnalyze scripts & passing tests
    Former-commit-id: a324f52084b66c43ea4fd9c330434a35d252ee02 [formerly a324f52084b66c43ea4fd9c330434a35d252ee02 [formerly dca91424795ab601160268b2b37b2a53e1b74a27 [formerly 5f323f43ec17a5a864796852ce929598831e6dc0] [formerly d34259bcf20014226e3c103d6f01c5848aee5d3a]]]
    Former-commit-id: 24c52496a5d6aa89604fa75103a3c438571d8834
    Former-commit-id: 17b7a6694085fc80bf2e5f2581e4e0d878cec57f
    Former-commit-id: 2271227e4fcfd3341a58a85a0ff2ba084f514553
    Former-commit-id: a98166ba046521b580e55368a75eb2e4fe16ea5d

--- tests/testthat/test-corrAnalyze.R
+++ tests/testthat/test-corrAnalyze.R
@@ -1,10 +1,6 @@
 test_that(desc = "Corr Analyze returns a numeric matrix", {
   expect_type( {
-    URL <- "https://rmapdb-data.s3.us-east-2.amazonaws.com/bigwigs/rseq-coverage-unstranded/SRX1025890_TC32_NT_DRIP.hg38.bw"
-    BW_FILE <- "SRX1025890.bw"
-    if (! file.exists(BW_FILE)) {
-      download.file(URL, destfile=BW_FILE)
-    }
+    BW_FILE <- "https://rmapdb-data.s3.us-east-2.amazonaws.com/bigwigs/rseq-coverage-unstranded/SRX1025890_hg38.bw"
     RSeqR::corrAnalyze(coverage = BW_FILE, genome="hg38")
   },
     "double"
