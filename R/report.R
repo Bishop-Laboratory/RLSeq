87858b9b6678611fb3de722b72c0aef7e9a076bb
millerh1 <millerh1@livemail.uthscsa.edu>
2021-09-20 18:59:05 -0500

    Working on HTML reports
    Former-commit-id: 025334e88180e6a52cddd63d1fc7b0394260d12c [formerly 025334e88180e6a52cddd63d1fc7b0394260d12c [formerly f4d972a2fd93c55ba916aad15240b8931194df08]]
    Former-commit-id: a5a631edc2415b011ceaf98fa69a184046e533f0
    Former-commit-id: 07ab3a1c351b0f6d9163e58200e9e507deb4587e

--- R/report.R
+++ R/report.R
@@ -1,29 +1,43 @@
-#' Generate report
+#' Generate an RLSeq Report
 #'
-#' Generates RLSeq Quality Report
-#'
-#' @param data A named list containing `annotated_peaks`, `feature_overlaps`,
-#' and `rlfs_results`
-#' @param outputFile A path indicating the report output HTML file.
+#' @param object An RLRanges object.
+#' @param output A path indicating the report output HTML file. Default: "rlreport.html"
+#' @param quiet If TRUE, messages are suppressed. Default: FALSE.
 #' @param ... Arguments passed to `rmarkdown::render()`
-#' @return NULL
+#' @return TRUE
 #' @examples
-#'
-#' RLSeq::makeReport(RLSeq::SRX1025890, outputFile = "report.html")
+#' 
+#' rlbase <- "https://rlbase-data.s3.amazonaws.com"
+#' pks <- file.path(rlbase, "peaks", "SRX1025890_hg38.broadPeak")
+#' cvg <- file.path(rlbase, "coverage", "SRX1025890_hg38.bw")
+#' rlr <- RLRanges(pks, coverage=cvg,  genome="hg38", mode="DRIP")
+#' rlr <- RLSeq(rlr)
+#' 
+#' report(rlr)
+#' 
 #' @importFrom dplyr %>%
 #' @importFrom rlang .data
 #' @export
-report <- function(data, outputFile = "report.html", ...) {
-  template <- system.file("Rmd", "report.Rmd", package = "RLSeq")
-  data <- data[names(data) %in% c(
-    "corr_data", "anno_data", "rlfs_data", "bam_stats",
-    "read_qc_data", "configlist", "total_peaks"
-  )]
-  rmarkdown::render(template,
-    params = data,
+report <- function(object, 
+                   output = "rlreport.html", 
+                   quiet=FALSE,
+                   ...) {
+  
+  # Get the template
+  template <- system.file("Rmd", "reportTemplate.Rmd", package = "RLSeq")
+
+  # Render template  
+  rmarkdown::render(
+    template,
+    params = list(
+      "object"=object
+    ),
     output_format = "html_document",
-    output_dir = normalizePath(dirname(outputFile)),
-    output_file = outputFile, ...
+    output_dir = normalizePath(dirname(output)),
+    output_file = output, 
+    ...
   )
-  return(NULL)
+  
+  # Return
+  return(TRUE)
 }
