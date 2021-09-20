72bc8278af576140bafc856900385518cb29e523
millerh1 <millerh1@livemail.uthscsa.edu>
2021-09-20 18:59:05 -0500

    Working on HTML reports
    Former-commit-id: 025334e88180e6a52cddd63d1fc7b0394260d12c [formerly 025334e88180e6a52cddd63d1fc7b0394260d12c [formerly a1b6da3ccb41de67a7320b1f4d67aa19dde42ac2 [formerly 34a880eabe1e49bac13f6b6caf40d0b7ddf0e007] [formerly f4d972a2fd93c55ba916aad15240b8931194df08]]]
    Former-commit-id: a5a631edc2415b011ceaf98fa69a184046e533f0
    Former-commit-id: 07ab3a1c351b0f6d9163e58200e9e507deb4587e
    Former-commit-id: 87858b9b6678611fb3de722b72c0aef7e9a076bb
    Former-commit-id: 4dd53653241043b01deeb30283485e2348efaece

--- R/RLSeq.R
+++ R/RLSeq.R
@@ -1,89 +1,48 @@
-#' Run RLSeq and generate report
+#' Run RLSeq
 #'
-#' Runs the full RLSeq pipeline and generates an analysis report in HTML format.
+#' A convenience function which runs the full RLSeq pipeline.
 #'
-#' @param peaks A GRanges object containing the R-loop ranges to check.
-#' @param genome UCSC genome which peaks were generated from.
-#' Only "hg38" and "mm10" currently available.
-#' Use RLSeq::liftUtil() to convert to the correct format, if needed.
-#' @param coverage (optional) The path to the bigWig file corresponding to `peaks`.
-#' If supplied, correlation analysis will be performed.
-#' @param outputFile A path indicating the report output HTML file.
-#' @param dataOnly If TRUE, the HTML report will not be created.
-#' @param sampleName The name to give to this sample in the report. Default: "User-supplied Sample".
-#' @param mode The mode type this sample belongs to. See options at RLSeq::modes.
-#' @param ... Arguments passed to `rmarkdown::render()`
-#' @return A Named List with the datasets passed to RLSeq::makeReport().
+#' @param object An RLRanges object.
+#' @param quiet If TRUE, messages are suppressed. Default: FALSE.
+#' @return An RLRanges object with all results available.
 #' @examples
-#'
-#' URL <- paste0(RLSeq:::RLBASE_BW_URL, "SRX1025890_hg38.bw")
-#' RLSeq::RLSeq(RLSeq::SRX1025890_peaks,
-#'   coverage = BW_FILE,
-#'   genome = "hg38", outputFile = "report.html"
-#' )
-#' file.remove(BW_FILE)
-#' @importFrom dplyr %>%
-#' @importFrom rlang .data
+#' 
+#' rlbase <- "https://rlbase-data.s3.amazonaws.com"
+#' pks <- file.path(rlbase, "peaks", "SRX1025890_hg38.broadPeak")
+#' cvg <- file.path(rlbase, "coverage", "SRX1025890_hg38.bw")
+#' rlr <- RLRanges(pks, coverage=cvg,  genome="hg38", mode="DRIP")
+#' rlr <- RLSeq(rlr)
+#' 
 #' @export
-RLSeq <- function(peaks, genome,
-                  coverage = NULL,
-                  sampleName = "User-supplied Sample",
-                  mode = "User-supplied Sample",
-                  outputFile = "report.html",
-                  dataOnly = FALSE,
-                  ...) {
+RLSeq <- function(object, quiet=FALSE) {
 
-  # Verify mode
-  if (mode != "User-supplied Sample") {
-    stopifnot(!mode %in% RLSeq::modes$mode)
-  }
+  if (! quiet) message("[1/6] RLFS Perm Test")
+  object <- analyzeRLFS(object, quiet = TRUE)
 
-  # Get data sets
-  message("[1] RLFS Perm Test...")
-  rlfsRes <- analyzeRLFS(peaks = peaks, genome = genome)
+  if (! quiet) message("[2/6] Predict Condition")
+  object <- predictCondition(object)
 
-  message("[2] Predict Condition...")
-  pred <- predictCondition(rlfsRes = rlfsRes)
+  if (! quiet) message("[3/6] Feature Enrichment Test")
+  object <- featureEnrich(object, quiet = TRUE)
 
-  message("[3] Feature Enrichment Test...")
-  featTest <- featureEnrich(peaks = peaks, genome = genome)
-
-  message("[4] Correlation Analysis...")
-  if (!is.null(coverage)) {
-    if (genome == "hg38") {
-      corrRes <- corrAnalyze(coverage = coverage, genome = genome)
+  if (! quiet) message("[4/6] Correlation Analysis")
+  if (object@metadata$coverage != "") {
+    if (GenomeInfoDb::genome(object)[1] == "hg38") {
+      object <- corrAnalyze(object)
     } else {
-      warning("Only hg38 is currently available for correlation analysis. Skipping...")
-      corrRes <- NA
+      warning("Only 'hg38' is available for correlation analysis. Skipping.")
+      Sys.sleep(1)
     }
   } else {
     message("No coverage provided... skipping.")
     corrRes <- NA
   }
 
-  message("[5] Gene Annotation...")
-  annoGenes <- geneAnnotation(peaks, genome = genome)
+  if (! quiet) message("[5/6] Gene Annotation")
+  object <- geneAnnotation(object, quiet = TRUE)
 
-  message("[6] R-loop Region Analysis...")
-  rlRegions <- rlRegionTest(peaks, genome = genome)
-
-  message("[7] Collating results...")
-  resLst <- list(
-    "rlfsRes" = rlfsRes,
-    "predictedCondition" = pred,
-    "featureTest" = featTest,
-    "corrRes" = corrRes,
-    "annoGenes" = annoGenes,
-    "RLoopRegions" = rlRegions,
-    "sampleName" = sampleName,
-    "mode" = mode,
-    "genome" = genome
-  )
-
-  if (!dataOnly) {
-    message("[8] Generating HTML Report...")
-    report(resLst, outputFile = outputFile)
-  }
-
-  return(resLst)
+  if (! quiet) message("[6/6] R-loop Region Analysis")
+  object <- rlRegionTest(object)
+  
+  return(object)
 }
