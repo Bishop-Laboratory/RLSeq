81cc567135009dd12f2b21f2c4b68929abe7ea79
millerh1 <millerh1@livemail.uthscsa.edu>
2021-09-22 21:26:26 -0500

    Windows issue in corrAnalyze fix
    Former-commit-id: ad3df3bae68c8aeba3b7f60791bf39a2423fec6f
    Former-commit-id: 44a0ffbc5b0cd8dfb511b188bcd7b47de3161793
    Former-commit-id: b738c63f7297d8de027775a54393702127cdc841
    Former-commit-id: 7b1b59800a764a2b5c2527ae5ee72a479830e28b

--- tests/testthat/test-corrAnalyze.R
+++ tests/testthat/test-corrAnalyze.R
@@ -2,11 +2,20 @@ test_that(desc = "Corr Analyze returns a numeric matrix", {
     # Load
     rlr <- readRDS(system.file("ext-data", "rlrsmall.rds", package = "RLSeq"))
 
-    # Plot the enrich
-    expect_s4_class(
-        RLSeq::corrAnalyze(
-            object = rlr
-        ),
-        "RLRanges"
-    )
+    # Get correlation. Should error if os is Windows.
+    if (.Platform$OS.type != "windows") {
+        expect_s4_class(
+            RLSeq::corrAnalyze(
+                object = rlr
+            ),
+            "RLRanges"
+        )
+    } else {
+        expect_error(
+            RLSeq::corrAnalyze(
+                object = rlr
+            )
+        )
+    }
+    
 })
