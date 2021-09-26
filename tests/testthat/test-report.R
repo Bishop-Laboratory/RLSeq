4488a499b7dbabf6d611d3ed0eb686fd9b7c7515
millerh1 <millerh1@livemail.uthscsa.edu>
2021-09-26 16:17:37 -0500

    Added local file test
    Former-commit-id: 610832fa85a8ae785b22669dac3151afe8087dc5
    Former-commit-id: 74b08d4b83141232c0e40ca482f080b48cf6f1ed
    Former-commit-id: 07c6cebadd6efc55f76334a939d2a1bcf943a4ef
    Former-commit-id: a6d7dfdbf26c5670a0bd35be33db03c5d114664d
    Former-commit-id: 52bbf4c1c9ea4d415fc8fa6f913a61a3e73fe3a7
    Former-commit-id: 03fac3349c9ba376eb26e3f5c45b07e7e20ddd19 [formerly 801076e8a65c1ca9a02e53b898283be33c6573b1]
    Former-commit-id: 5015f34aca54c1240f70a5fee51a46afeecbdc36

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -9,4 +9,18 @@ test_that(desc = "RLSeq report", {
             quiet = TRUE
         )
     )
+    
+    # Download coverage locally and try with local file
+    coverage <- tempfile(tmpdir = ".", fileext = ".bw")
+    download.file(rlr@metadata$coverage, destfile = coverage, quiet = TRUE)
+    rlrloc <- rlr
+    coverage <- file.path(normalizePath(dirname(coverage)), 
+                          basename(coverage))
+    rlrloc@metadata$coverage <- coverage
+    expect_true(
+        RLSeq::report(
+            object = rlrloc,
+            quiet = TRUE
+        )
+    )
 })
