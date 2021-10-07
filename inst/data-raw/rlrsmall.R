6f833b4c08357b49150525d38b92f8c8ddae18be
millerh1 <millerh1@livemail.uthscsa.edu>
2021-10-06 23:00:57 -0500

    Fixing tree more
    Former-commit-id: c673a93f20e35fb74a471d360ccedbf843986592
    Former-commit-id: f4ec985ef1b64f6f11f2bb3f790feae73362082e

--- inst/data-raw/rlrsmall.R
+++ inst/data-raw/rlrsmall.R
@@ -6,7 +6,7 @@ pks <- file.path(rlbase, "peaks", "SRX7671349_hg38.broadPeak")
 cvg <- file.path(rlbase, "coverage", "SRX7671349_hg38.bw")
 
 # Get RLRanges object
-rlr <- RLRanges(pks,
+rlr <- RLSeq::RLRanges(pks,
     coverage = cvg, genome = "hg38", label="NEG",
     mode = "RDIP", sampleName = "RDIP-Seq +RNH1"
 )
@@ -15,4 +15,5 @@ rlr <- RLRanges(pks,
 rlr <- RLSeq::RLSeq(rlr)
 
 # Save
+dir.create("inst/ext-data/", showWarnings = FALSE)
 saveRDS(rlr, file = "inst/ext-data/rlrsmall.rds", compress = "xz")
