4ed9eb95d4d1b188ad149a152b1d94cb70731ef0
millerh1 <millerh1@livemail.uthscsa.edu>
2021-10-07 16:55:15 -0500

    Updated RLSeq to be faster for bioconductor
    Former-commit-id: e4f576eed2f9023b50ee693abcf4e3b03d5ce06e
    Former-commit-id: bdfe302a3020622c9acc5a96680479b2bd9dbbea [formerly 9a43440d2f479104f16716dec15856bbfca42a95]
    Former-commit-id: 89c7222deccb1dcec67cadcf6a52f6524050a420

--- R/RLSeq.R
+++ R/RLSeq.R
@@ -4,60 +4,69 @@
 #'
 #' @param object An RLRanges object.
 #' @param quiet If TRUE, messages are suppressed. Default: FALSE.
+#' @param skip Analysis steps to skip. 
+#' Default: NULL. See details for options.
 #' @param ... Arguments passed to analyzeRLFS. See [analyzeRLFS].
-#' @return An RLRanges object with all results available.
+#' @return An RLRanges object with results available.
 #' @details
 #'
-#' The \code{RLSeq()} function does all of the following:
+#' The `RLSeq()` function does all of the following by default:
 #'
-#' \enumerate{
-#'   \item RLFS Perm Test. Runs the \code{analyzeRLFS()} function to test the
+#' 1. **RLFS Perm Test**. Runs the `analyzeRLFS()` function to test the
 #'   enrichment of user-supplied ranges within R-loop-forming sequences.
-#'   \item Predict Condition. Runs the \code{predictCondition()} function to
+#'   *Cannot be skipped.*
+#' 2. **Predict Condition**. Runs the `predictCondition()` function to
 #'   predict whether the user-supplied sample robustly maps R-loops or not.
-#'   \item Feature enrichment test. Runs the \code{featureEnrich()} function to
-#'   test the enrichment of user-supplied ranges within R-loop-relevant genomic features.
-#'   \item Correlation Analysis. Runs the \code{corrAnalyze()} function to test
-#'   the correlation of user-supplied R-loop signal with other samples in RLBase around
-#'   "gold-standard" R-loop regions.
-#'   \item Gene annotation. Runs the \code{geneAnnotation()} function to find the overlap
-#'   of genes with the user-supplied ranges.
-#'   \item R-loop Region Analysis. Runs the \code{rlRegionTest()} function to find
-#'   the overlap of user-supplied ranges with consensus R-loop sites (RL-Regions).
-#' }
-#'
+#'   *Cannot be skipped.* [RLHub::models].
+#' 3. **Feature enrichment test**. Runs the `featureEnrich()` function to
+#'   test the enrichment of user-supplied ranges within R-loop-relevant 
+#'   genomic features. Skip with `skip="featureEnrich"`. See also
+#'   [RLHub::annotations].
+#' 4. **Correlation Analysis**. Runs the `corrAnalyze()` function to test
+#'   the correlation of user-supplied R-loop signal with other samples in 
+#'   RLBase around "gold-standard" R-loop regions. 
+#'   Skip with `skip="corrAnalyze"`. See also [RLHub::gs_signal].
+#' 5. **Gene annotation**. Runs the `geneAnnotation()` function to find the overlap
+#'   of genes with the user-supplied ranges. Skip with `skip="geneAnnotation"`.
+#' 6. **R-loop Region Analysis**. Runs the `rlRegionTest()` function to find
+#'   the overlap of user-supplied ranges with consensus R-loop sites 
+#'   (RL-Regions). Skip with `skip="rlRegionTest"`. See also [RLHub::rlregions].
+#' 
 #' @examples
 #'
 #' # Example RLRanges
 #' rlr <- readRDS(system.file("ext-data", "rlrsmall.rds", package = "RLSeq"))
 #'
 #' # Run RLSeq
-#' # `useMask=FALSE` & `ntime=10` for demonstration purposes only.
-#' # For an accurate analysis, use `rlr <- RLSeq(rlr)`
-#' rlr <- RLSeq(rlr, useMask = FALSE, ntimes = 10)
+#' # `useMask=FALSE`, `ntime=10`, and `skip=` for demonstration purposes here.
+#' rlr <- RLSeq(
+#'     rlr, useMask = FALSE, ntimes = 10,
+#'     skip=c('featureEnrich', 'corrAnalyze', 'geneAnnotation', 'rlRegionTest')
+#' )
+#' 
 #' @export
-RLSeq <- function(object, quiet = FALSE, ...) {
+RLSeq <- function(object, quiet = FALSE, skip=NULL, ...) {
     if (!quiet) message("[1/6] RLFS Perm Test")
     object <- analyzeRLFS(object, quiet = TRUE, ...)
-
+    
     if (!quiet) message("[2/6] Predict Condition")
     object <- predictCondition(object)
-
+    
     if (!quiet) message("[3/6] Feature Enrichment Test")
-    if (GenomeInfoDb::genome(object)[1] %in% c("hg38", "mm10")) {
-        object <- featureEnrich(object, quiet = TRUE)
-    } else {
-        warning(
-            "RLSeq only contains built-in annotations for 'hg38' and 'mm10'",
-            " genomes. Please lift-over or run featureEnrich() separately ",
-            "with custom annotations. Skipping."
-        )
-        Sys.sleep(2)
+    if (! "featureEnrich" %in% skip) {
+        if (GenomeInfoDb::genome(object)[1] %in% c("hg38", "mm10")) {
+            object <- featureEnrich(object, quiet = TRUE)
+        } else {
+            warning(
+                "RLSeq only contains built-in annotations for 'hg38' and",
+                "'mm10' genomes. Please lift-over or run featureEnrich() ",
+                "separately with custom annotations. Skipping."
+            )
+        }
     }
-
-
+    
     if (!quiet) message("[4/6] Correlation Analysis")
-    if (object@metadata$coverage != "") {
+    if (object@metadata$coverage != "" & ! "corrAnalyze" %in% skip) {
         if (GenomeInfoDb::genome(object)[1] == "hg38") {
             if (.Platform$OS.type != "windows") {
                 object <- corrAnalyze(object)
@@ -75,31 +84,32 @@ RLSeq <- function(object, quiet = FALSE, ...) {
                 "Only 'hg38' genome ranges are available",
                 " for correlation analysis. Skipping."
             )
-            Sys.sleep(2)
         }
     } else {
         message("No coverage provided... skipping.")
         corrRes <- NA
     }
-
+    
     if (!quiet) message("[5/6] Gene Annotation")
-    objectanno <- try(geneAnnotation(object), silent = TRUE)
-    if ("try-error" %in% class(objectanno)) {
-        warning(objectanno)
-    } else {
-        object <- objectanno
+    if (! "geneAnnotation" %in% skip) {
+        objectanno <- try(geneAnnotation(object), silent = TRUE)
+        if ("try-error" %in% class(objectanno)) {
+            warning(objectanno)
+        } else {
+            object <- objectanno
+        }
     }
-
+    
     if (!quiet) message("[6/6] R-loop Region Analysis")
-    if (GenomeInfoDb::genome(object)[1] == "hg38") {
-        object <- rlRegionTest(object)
-    } else {
-        warning(
-            "Only 'hg38' genome ranges",
-            " are available for RL-region analysis. Skipping."
-        )
-        Sys.sleep(1)
+    if (! "rlRegionTest" %in% skip) {
+        if (GenomeInfoDb::genome(object)[1] == "hg38") {
+            object <- rlRegionTest(object)
+        } else {
+            warning(
+                "Only 'hg38' genome ranges",
+                " are available for RL-region analysis. Skipping."
+            )
+        }
     }
-
     return(object)
 }
